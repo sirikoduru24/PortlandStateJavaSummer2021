40b29835d1d8484bfbb51674fd12ad4a42fa3e63
sirikoduru24 <81694764+sirikoduru24@users.noreply.github.com>
2021-07-01 13:58:41 -0700

    Starter files for project1

---
+++ apptbook/src/test/java/edu/pdx/cs410J/siri4/AppointmentTest.java
@@ -0,0 +1,34 @@
+package edu.pdx.cs410J.siri4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Unit tests for the {@link Appointment} class.
+ *
+ * You'll need to update these unit tests as you build out your program.
+ */
+public class AppointmentTest {
+
+  @Test
+  void getBeginTimeStringNeedsToBeImplemented() {
+    Appointment appointment = new Appointment();
+    assertThrows(UnsupportedOperationException.class, appointment::getBeginTimeString);
+  }
+
+  @Test
+  void initiallyAllAppointmentsHaveTheSameDescription() {
+    Appointment appointment = new Appointment();
+    assertThat(appointment.getDescription(), containsString("not implemented"));
+  }
+
+  @Test
+  void forProject1ItIsOkayIfGetBeginTimeReturnsNull() {
+    Appointment appointment = new Appointment();
+    assertThat(appointment.getBeginTime(), is(nullValue()));
+  }
+
+}
